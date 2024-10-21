a84014cc69d4932ac8cf21672b05c5dc7cc4642e
lpesternikova <lpesternikova@gmail.com>
2024-10-21 15:46:57 +0500

    task 2 done

--- task2.py
+++ task2.py
@@ -2,3 +2,14 @@
 # которая найдёт сумму элементов списка, стоящих на нечётной позиции.
 #  Пример:
 # [2, 3, 5, 9, 3] -> на нечётных позициях элементы 3 и 9, ответ: 12
+
+spisok = str(input())
+b = 0
+result = 0 
+
+for elem in spisok:
+    #print(spisok.index(elem))
+    b = spisok.index(elem)
+    if b % 2 != 0:
+        result += int(elem)
+print(result)
