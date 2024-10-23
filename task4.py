b6175c7eaaa42deab70c1f7f24791f07e496589f
lpesternikova <lpesternikova@gmail.com>
2024-10-23 17:13:38 +0500

    task4 done

--- task4.py
+++ task4.py
@@ -4,10 +4,29 @@
 # Для списка: [1, 2, "3", "4", True, 5.5]
 # Ответ:  {int: [1, 2, 5], float: [5.5], str: ["3", "4"], bool: [True]}
 
-list1 = (1, 3, True, False, "name", "6", 6.8)
-dict = []
+list1 = (1, 3, True, False, "str", "6", 6.8)
+dict = {}
 
 for elem in list1:
-    if elem == int:
-        dict[int] = elem
-        print(dict)
+    if type(elem) == str:
+        if str not in dict:
+            dict["str"] = [elem]
+        else:
+            dict["str"].append(elem)
+    elif type(elem) == int:
+        if "int" not in dict:
+            dict["int"] = [elem]
+        else:
+            dict["int"].append(elem)
+    elif type(elem) == bool:
+        if "bool" not in dict:
+            dict["bool"] = [elem]
+        else:
+            dict["bool"].append(elem)
+    elif type(elem) == float:
+        if "float" not in dict:
+            dict["float"] = [elem]
+        else:
+            dict["float"].append(elem)
+
+print(dict)
