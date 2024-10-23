8ba729ce01a75a56e1c92e05977a316924937cbb
lpesternikova <lpesternikova@gmail.com>
2024-10-23 18:18:18 +0500

    task5 done

--- task5.py
+++ task5.py
@@ -2,4 +2,24 @@
 # количество встречаемых слов и вернуть 10 самых частых.
 # Не учитывать знаки препинания и регистр символов.
 
-str1 = ""
+str1 = "САМАЯ Длинная строка в мире, мире строка f hj hj yu yu ooo ooo ooo ooo ooo ooo жажаж жажаж жажаж строка !строка, строкка самая длинная ?или может не самая или не длинная."
+str2 = str1.replace(",","") 
+str3 = str2.replace(".","") 
+str4 = str3.replace("!","") 
+str5 = str4.replace("?","")
+str6 = str5.lower()
+
+list1 = str6.split(" ")
+dict = {}
+
+for elem in list1:
+    if elem in dict:
+        dict[elem] += 1
+    else:
+        dict[elem] = 1
+
+#
+#print(dict)
+
+sorted_dict = sorted(dict.items(), key = lambda item: item[1], reverse=True)
+print(sorted_dict[0:10:])
